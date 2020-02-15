e26ec6e3925197a9c9ae5fbb07a98a935ffb87c5
草梅友仁 <996881204@qq.com>
2020-02-15 17:40:12 +0800

    chore: 修改了更新日志的生成类型

--- writer-opts.js
+++ writer-opts.js
@@ -61,30 +61,30 @@ function getWriterOpts() {
       })
 
       if (emojis) {
-        if (commit.type === `feat`) {
-          commit.type = `✨ Features`
-        } else if (commit.type === `fix`) {
-          commit.type = `🐛 Bug Fixes`
-        } else if (commit.type === `perf`) {
-          commit.type = `⚡ Performance Improvements`
-        } else if (commit.type === `revert`) {
-          commit.type = `⏪ Reverts`
-        } else if (commit.type === `docs`) {
-          commit.type = `📝 Documentation`
-        } else if (commit.type === `style`) {
-          commit.type = `💄 Styles`
-        } else if (commit.type === `refactor`) {
-          commit.type = `♻ Code Refactoring`
-        } else if (commit.type === `test`) {
-          commit.type = `✅ Tests`
-        } else if (commit.type === `build`) {
-          commit.type = `👷‍ Build System`
-        } else if (commit.type === `ci`) {
-          commit.type = `🔧 Continuous Integration`
+        if (commit.type === 'feat') {
+          commit.type = '✨ Features | 新功能'
+        } else if (commit.type === 'fix') {
+          commit.type = '🐛 Bug Fixes | Bug 修复'
+        } else if (commit.type === 'perf') {
+          commit.type = '⚡ Performance Improvements | 性能优化'
+        } else if (commit.type === 'refactor') {
+          commit.type = '♻ Code Refactoring | 代码重构'
+        } else if (commit.type === 'revert' || commit.revert) {
+          commit.type = '⏪ Reverts | 回退'
+        } else if (commit.type === 'style') {
+          commit.type = '💄 Styles | 风格'
+        } else if (commit.type === 'test') {
+          commit.type = '✅ Tests | 测试'
+        } else if (commit.type === 'build') {
+          commit.type = '👷‍ Build System | 构建'
+        } else if (commit.type === 'ci') {
+          commit.type = '🔧 Continuous Integration | CI 配置'
         } else if (commit.type === 'chore') {
-          commit.type = '🎫 Chores'
-        } else if (discard) {
+          commit.type = '🎫 Chores | 其他更新'
+        } else if (discard || !commit.type) {
           return
+        } else if (commit.type === 'docs') {
+          commit.type = '📝 Documentation | 文档'
         }
       } else {
         if (commit.type === `feat`) {
